a1107d52dd77c928c4fcdcd01b00f8d73bac31f8
Greg Cox <gcox@mozilla.com>
2018-08-30 21:43:13 +0000

    Update setup.py to build packages with the git commit hash

--- setup.py
+++ setup.py
@@ -6,10 +6,37 @@
 """ Packaging script """
 
 import os
+import subprocess
 from setuptools import setup
 
 NAME = 'iamvpnlibrary'
 VERSION = '0.8.2'
+
+
+def git_version():
+    """ Return the git revision as a string """
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for envvar in ['SYSTEMROOT', 'PATH']:
+            val = os.environ.get(envvar)
+            if val is not None:
+                env[envvar] = val
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                               env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        git_revision = out.strip().decode('ascii')
+    except OSError:
+        git_revision = u"Unknown"
+
+    return git_revision
 
 
 def read(fname):
@@ -22,7 +49,8 @@ setup(
     version=VERSION,
     author='Greg Cox',
     author_email='gcox@mozilla.com',
-    description=('Mozilla-specific user authorization for VPN access'),
+    description=('Mozilla-specific user authorization for VPN access\n' +
+                 'This package is built upon commit ' + git_version()),
     license='MPL',
     keywords='mozilla ldap',
     url='https://github.com/mozilla-it/iamvpnlibrary',
